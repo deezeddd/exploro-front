4deeb55515029f23fd37a7eeb5e9bfdd194d4086
Deezeddd <kalpved123@gmail.com>
2024-06-06 23:49:25 +0530

    removed comments

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -10,18 +10,13 @@ const handler = NextAuth({
   },
     providers:[
       CredentialProvider({
-        // The name to display on the sign in form (e.g. "Sign in with...")
         name: "Credentials",
-        // `credentials` is used to generate a form on the sign in page.
-        // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-        // e.g. domain, username, password, 2FA token, etc.
-        // You can pass any HTML attribute to the <input> tag through the object.
+
         credentials: {
           email: { label: "Username", type: "text", placeholder: "jsmith" },
           password: { label: "Password", type: "password" }
         },
         async authorize(credentials, req) {
-          // Add logic here to look up the user from the credentials supplied
           let flag = null;
           const {email , password } = credentials as {
             email : string,
